d3a93302f58d99d396adfd2ee5c78b1c3b89944f
rahul <ra9853044@gmail.com>
2023-11-14 21:38:19 +0530

    created note row and showed dummy notes

--- app/src/main/java/com/example/jetnote/sceen/NoteScreen.kt
+++ app/src/main/java/com/example/jetnote/sceen/NoteScreen.kt
@@ -1,12 +1,19 @@
 package com.example.jetnote.sceen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -22,7 +29,9 @@ import androidx.compose.ui.unit.dp
 import com.example.jetnote.R
 import com.example.jetnote.components.NoteInputText
 import com.example.jetnote.components.NotesButton
+import com.example.jetnote.data.DataSource
 import com.example.jetnote.model.Note
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -82,11 +91,61 @@ fun NoteScreen(
                 text = "Save",
                 onClick = {
                           if(title.value.isNotEmpty() && description.value.isNotEmpty()) {
+                              addNote(Note(title=title.value, description = description.value))
                               title.value=""
                               description.value=""
                           }
                 },
                 modifier = Modifier.padding(top = 10.dp)
+            )
+        }
+        Divider(
+            modifier = Modifier.padding(
+                10.dp
+            )
+        )
+        LazyColumn{
+            items(notes) {
+                NoteRow(
+                    onNoteClicked = removeNote,
+                    note=it
+                )
+            }
+        }
+    }
+}
+@Composable
+private fun NoteRow(
+    modifier: Modifier=Modifier,
+    onNoteClicked: (Note) -> Unit,
+    note: Note
+) {
+    Card(
+        shape = RoundedCornerShape(topEnd = 10.dp, bottomStart = 10.dp),
+        modifier = modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .clickable {
+                onNoteClicked(note)
+            }
+    ) {
+        Column(
+            modifier=modifier.padding(
+                horizontal = 14.dp,
+                vertical = 6.dp
+            )
+        ) {
+            Text(
+                text = note.title,
+                style=MaterialTheme.typography.titleLarge
+                )
+            Text(
+                text = note.description,
+                style=MaterialTheme.typography.titleMedium
+                )
+            Text(
+                text = note.entryDate.format(DateTimeFormatter.ofPattern("EEE, d MMM")),
+                style = MaterialTheme.typography.titleSmall
             )
         }
     }
@@ -96,7 +155,7 @@ fun NoteScreen(
 @Composable
 fun NoteScreenPreview() {
     NoteScreen(
-        notes = emptyList(),
+        notes = DataSource.notes,
         addNote = {},
         removeNote = {}
     )
