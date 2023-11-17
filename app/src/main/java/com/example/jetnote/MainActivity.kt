473c72a8148c98388f85e2c59e911696daf702f8
rahul <ra9853044@gmail.com>
2023-11-17 22:45:30 +0530

    added and removed notes

--- app/src/main/java/com/example/jetnote/MainActivity.kt
+++ app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetnote.data.DataSource
+import com.example.jetnote.model.Note
 import com.example.jetnote.sceen.NoteScreen
 import com.example.jetnote.ui.theme.JetNoteTheme
 
@@ -24,8 +27,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val notes:MutableList<Note> = remember {
+                        mutableStateListOf()
+                    }
+                    notes.addAll(DataSource.notes)
                     NoteScreen(
-                        notes = DataSource.notes
+                        notes = notes,
+                        addNote = {
+                            notes.add(it)
+                        },
+                        removeNote={
+                            notes.remove(it)
+                        }
                     )
                 }
             }
--- app/src/main/java/com/example/jetnote/data/DataSource.kt
+++ app/src/main/java/com/example/jetnote/data/DataSource.kt
@@ -3,7 +3,7 @@ package com.example.jetnote.data
 import com.example.jetnote.model.Note
 
 object DataSource {
-    val notes:List<Note> = listOf(
+    val notes:MutableList<Note> = mutableListOf(
         Note(title="Happy Holidays", description = "Spend holidays with family"),
         Note(title="Happy Diwali", description = "Spend Diwali with family") ,
         Note(title="Happy Bhaiya Dooj", description = "Spend Bhaiya Dooj with family")
