0c14edd8fc417934be08ff6f2d5bb9c47d7f8313
rahul <ra9853044@gmail.com>
2023-11-25 13:45:32 +0530

    added suspend and flow to dao functions

--- app/src/main/java/com/example/jetnote/data/NoteDatabaseDao.kt
+++ app/src/main/java/com/example/jetnote/data/NoteDatabaseDao.kt
@@ -7,24 +7,25 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.example.jetnote.model.Note
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDatabaseDao {
     @Query("Select * from notes_tbl")
-    fun getNotes():List<Note>
+    suspend fun getNotes(): Flow<List<Note>>
 
     @Query("Select * from notes_tbl where id=:id")
-    fun getNoteById(id:String):Note
+    suspend fun getNoteById(id:String):Note
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertNote(note: Note)
+    suspend fun insertNote(note: Note)
 
     @Delete
-    fun deleteNote(note:Note)
+    suspend fun deleteNote(note:Note)
 
     @Query("Delete from notes_tbl")
-    fun deleteAllNotes()
+    suspend fun deleteAllNotes()
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
-    fun updateNote(note:Note)
+    suspend fun updateNote(note:Note)
 }
