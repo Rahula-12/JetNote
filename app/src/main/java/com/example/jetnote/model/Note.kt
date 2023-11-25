0e28bc6d1f039c83ae81e0c4a1b7dcffb1bde879
rahul <ra9853044@gmail.com>
2023-11-25 13:37:48 +0530

    set up room database, entities and dao

--- app/src/main/java/com/example/jetnote/model/Note.kt
+++ app/src/main/java/com/example/jetnote/model/Note.kt
@@ -1,12 +1,20 @@
 package com.example.jetnote.model
 
 import android.icu.text.CaseMap.Title
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 import java.util.UUID
 
+@Entity(tableName = "notes_tbl")
 data class Note(
+    @PrimaryKey
     val id:UUID=UUID.randomUUID(),
+    @ColumnInfo(name = "notes_title")
     val title: String,
+    @ColumnInfo(name="notes_description")
     val description:String,
+    @ColumnInfo(name="notes_entryDate")
     val entryDate:LocalDateTime= LocalDateTime.now()
 )
